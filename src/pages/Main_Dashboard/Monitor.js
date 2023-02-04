1a9b11f173f568d3be0ca52e6aa4299ceae81204
tech-911 <babatundeolatunji74@gmail.com>
2023-02-04 09:44:07 +0100

    added link

--- src/pages/Main_Dashboard/Monitor.js
+++ src/pages/Main_Dashboard/Monitor.js
@@ -4,7 +4,15 @@ import { data } from "./graphData";
 
 class Monitor extends Component {
   render() {
-    return <div className="flex flex-col h-[100%] w-[100%]"></div>;
+    return (
+      <div className="flex flex-row h-[100%] w-[100%]">
+        <p className="text-[white] text-[1rem] ml-10 mt-10">Link:</p>
+        <h1 className="text-[white] text-[1rem] ml-2 mt-10">
+          {" "}
+          https://octoprint.remote3dprinter.info/
+        </h1>
+      </div>
+    );
   }
 }
 export default Monitor;
